05c7733c0192b3dca224055b3ec1b6a366f0e339
unknown <asiazmudziak@gmail.com>
2021-04-30 01:52:28 +0200

    app content

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const app = express();
 
 app.use((req, res, next) => {
-  res.show = (name) => {
+  res.show = name => {
     res.sendFile(path.join(__dirname + `/views/${name}`));
   };
   next();
 });
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use('/user', (req, res, next) => {
+  res.show('forbidden.html');
+});
 
 app.get('/', (req, res) => {
   res.show('home.html');
@@ -24,6 +26,14 @@ app.get('/about', (req, res) => {
   res.show('about.html');
 });
 
+app.get('/page-not-found.png', (req, res) => {
+  res.sendFile(path.join(__dirname + '/page-not-found.png'));
+});
+
+app.use((req, res) => {
+  res.status(404).show('404.html');
+});
+
 app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+  console.log('Server is running on port: 8000');
 });
